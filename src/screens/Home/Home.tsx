6dc4a9c47eca2d3411790b0b04f549bbc95506d7
pasqualintosh <pasqsac@yahoo.it>
2021-08-25 15:16:28 +0200

    switched to a spa in order to get confident with ts - added localstorage support (hook). tbd testing libraries

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,9 +1,34 @@
-type HomeProps = {
-  message?: string;
-};
+import CreatePeople from '../../components/CreatePeople/CreatePeople';
+import ShowPeople from './../../components/ShowPeople/ShowPeople';
+import useLocalStorage from './../../helpers/useLocalStorage';
 
-const Home = ({ message = 'Home' }: HomeProps): JSX.Element => (
-  <div>{message}</div>
-);
+interface IProps {
+  message?: string;
+}
+
+export interface IState {
+  people: {
+    name: string;
+    age: number;
+    url: string;
+    note?: string;
+  }[];
+}
+
+const Home: React.FC<IProps> = (): JSX.Element => {
+  // const [people, setPeople] = useState<IState['people']>(new Array());
+
+  const defaultPeople: IState['people'] = [];
+  const [people, setPeople] = useLocalStorage('people', defaultPeople);
+
+  return (
+    <div>
+      <h1>people in my list</h1>
+      <ShowPeople people={people} />
+      <hr />
+      <CreatePeople people={people} setPeople={setPeople} />
+    </div>
+  );
+};
 
 export default Home;
