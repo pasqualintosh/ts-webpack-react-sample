ff8bfe277983c1094d6775d3a252bafd7fe01149
Pasquale Sacco <pasqsac@yahoo.it>
2021-10-27 12:09:46 +0200

    first commit of crossfit app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import { WodProvider } from './providers/WodProvider';
 import Home from './screens/Home/Home';
+import Scores from './screens/Home/Scores';
 
-const App = () => {
+const App: React.FC = (): JSX.Element => {
   return (
     <Router>
-      <Switch>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
+      <WodProvider>
+        <Layout>
+          <Switch>
+            <Route path="/scores/">
+              <Scores />
+            </Route>
+            <Route path="/profile/">
+              <>
+                <h3>TBD</h3>
+              </>
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Layout>
+      </WodProvider>
     </Router>
   );
 };
---
+++ src/components/Layout.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+interface IProps {}
+interface IState {}
+
+const Layout: React.FC<IProps> = ({ children }): JSX.Element => {
+  return (
+    <>
+      <ul
+        style={{
+          display: 'flex',
+          justifyContent: 'space-around',
+          alignItems: 'center',
+          listStyleType: 'none',
+        }}>
+        <li>
+          <Link to={'/'}>generate</Link>
+        </li>
+        <li>
+          <Link to={'/scores/'}>your scores</Link>
+        </li>
+        <li>
+          <Link to={'/profile/'}>profile</Link>
+        </li>
+      </ul>
+      <span
+        style={{
+          marginTop: '40px',
+          display: 'flex',
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        {children}
+      </span>
+    </>
+  );
+};
+
+export default Layout;
